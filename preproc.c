f2fa3bdd22191630c123a5715f843765991f46c3
rofl0r <rofl0r@users.noreply.github.com>
2019-07-07 23:33:10 +0100

    ignore pragmas

--- preproc.c
+++ preproc.c
@@ -1022,7 +1022,7 @@ int parse_file(struct cpp *cpp, FILE *f, const char *fn, FILE *out) {
 #define skip_conditional_block (if_level > if_level_active)
 
 	const char *macro_name = 0;
-	static const char* directives[] = {"include", "error", "warning", "define", "undef", "if", "elif", "else", "ifdef", "ifndef", "endif", 0};
+	static const char* directives[] = {"include", "error", "warning", "define", "undef", "if", "elif", "else", "ifdef", "ifndef", "endif", "pragma", 0};
 	while((ret = tokenizer_next(&t, &curr)) && curr.type != TT_EOF) {
 		newline = curr.column == 0;
 		if(newline) {
@@ -1117,6 +1117,11 @@ int parse_file(struct cpp *cpp, FILE *f, const char *fn, FILE *out) {
 				break;
 			case 10: // endif
 				set_level(if_level-1, -1);
+				break;
+			case 11: // pragma
+				ret = tokenizer_read_until(&t, "\n", 1);
+				if(!ret) goto unknown_error;
+				break;
 			default:
 				break;
 			}
@@ -1144,6 +1149,7 @@ int parse_file(struct cpp *cpp, FILE *f, const char *fn, FILE *out) {
 		}
 	}
 	if(!ret) {
+unknown_error:
 		error("unknown", &t, &curr);
 	}
 	if(if_level) {
