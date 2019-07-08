f054262c5ed637985fd6ae683bd086b5fa32c532
rofl0r <rofl0r@users.noreply.github.com>
2019-07-08 01:19:06 +0100

    cppmain: support -I include directives

--- cppmain.c
+++ cppmain.c
@@ -1,9 +1,36 @@
 #include "preproc.h"
+#include <string.h>
+#include <unistd.h>
+
+static int usage(char *a0) {
+	fprintf(stderr,
+			"example preprocessor\n"
+			"usage: %s [-I includedir...] file\n"
+			"if no filename or '-' is passed, stdin is used.\n"
+			, a0);
+	return 1;
+}
 
 int main(int argc, char** argv) {
+	int c;
 	struct cpp* cpp = cpp_new();
-	int ret = cpp_run(cpp, stdin, stdout, "stdin");
+	while ((c = getopt(argc, argv, "I:")) != EOF) switch(c) {
+	case 'I': cpp_add_includedir(cpp, optarg); break;
+	default: return usage(argv[0]);
+	}
+	char *fn = "stdin";
+	FILE *in = stdin;
+	if(argv[optind] && strcmp(argv[optind], "-")) {
+		fn = argv[optind];
+		in = fopen(fn, "r");
+		if(!in) {
+			perror("fopen");
+			return 1;
+		}
+	}
+	int ret = cpp_run(cpp, in, stdout, fn);
 	cpp_free(cpp);
+	if(in != stdin) fclose(in);
 	return !ret;
 }
 
