fe4f8aac6e45798830e172ab41ed87e4ce3e3dca
rofl0r <rofl0r@users.noreply.github.com>
2019-07-10 04:27:19 +0100

    ignore #line directive

--- preproc.c
+++ preproc.c
@@ -1137,7 +1137,7 @@ int parse_file(struct cpp *cpp, FILE *f, const char *fn, FILE *out) {
 #define skip_conditional_block (if_level > if_level_active)
 
 	const char *macro_name = 0;
-	static const char* directives[] = {"include", "error", "warning", "define", "undef", "if", "elif", "else", "ifdef", "ifndef", "endif", "pragma", 0};
+	static const char* directives[] = {"include", "error", "warning", "define", "undef", "if", "elif", "else", "ifdef", "ifndef", "endif", "line", "pragma", 0};
 	while((ret = tokenizer_next(&t, &curr)) && curr.type != TT_EOF) {
 		newline = curr.column == 0;
 		if(newline) {
@@ -1234,7 +1234,8 @@ int parse_file(struct cpp *cpp, FILE *f, const char *fn, FILE *out) {
 			case 10: // endif
 				set_level(if_level-1, -1);
 				break;
-			case 11: // pragma
+			case 11: // line
+			case 12: // pragma
 				ret = tokenizer_read_until(&t, "\n", 1);
 				if(!ret) goto unknown_error;
 				break;
