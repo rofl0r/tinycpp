6687e30b11f73fe715cac76c83ac59d18ce89617
rofl0r <rofl0r@users.noreply.github.com>
2019-07-11 01:04:52 +0100

    tokenizer: fix another float parsing regression
    in the expression t2.f, the code considered the "2.f" is part of
    a float literal, thus it continued fetching more input, resulting
    in an uknown token type.
    
    fixes self-compilation.

--- tokenizer.c
+++ tokenizer.c
@@ -179,7 +179,7 @@ static int is_valid_float_until(const char*s, const char* until) {
 		} else return 0;
 		++s;
 	}
-	return got_digits;
+	return got_digits | (got_dot << 1);
 }
 
 static int is_oct_int_literal(const char *s) {
@@ -436,7 +436,7 @@ int tokenizer_next(struct tokenizer *t, struct token* out) {
 			} else if(is_plus_or_minus(c) && s > t->buf+1 &&
 				  (s[-1] == 'E' || s[-1] == 'e') && is_valid_float_until(t->buf, s-1)) {
 				goto process_char;
-			} else if(c == '.' && s != t->buf && (isdigit(s[-1]) || (s == t->buf+1 && is_plus_or_minus(s[-1])))) {
+			} else if(c == '.' && s != t->buf && is_valid_float_until(t->buf, s) == 1) {
 				goto process_char;
 			} else if(c == '.' && s == t->buf) {
 				int jump = 0;
